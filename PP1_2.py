f63fa0b695c1a2a6beccfe507206f988d44bcbcc
DhakshayiniChandrasekaran-ICD2O1-01 <dhakshayini.chandrasekaran@student.tdsb.on.ca>
2024-10-25 01:27:04 +0000

    hi

--- PP1_2.py
+++ PP1_2.py
@@ -1,16 +1,21 @@
+'''
 Lesson 1.2- Variables, operators, concatention
 September/16/2024
 Dhakshayini Chandrasekaran
 #Write Assignment code here
+''' 
 
 def q1():
+    #Write Assignment code here
     name = alex
     print(name)
+
 def q2():
     num1 = 3
     num2 = 5
     num3 = 2
     print(num1 + num2 + num3)
+
 def q3():
     num1 = 21
     num2 = 4
@@ -21,11 +26,13 @@ def q4():
     float2 = 3.0
     exact_quotient = float1 / float2
     print(exact_quotient)
+
 def q5():
     message = "Hello World"
     print(message)
     message = "Goodbye World"
     print(message)
+
 def q6():
     bool_var = True
     print(bool_var)
